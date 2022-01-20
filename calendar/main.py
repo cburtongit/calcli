6eec7e9f2b2a7bdd1e603d7ee987b2c047c8db7d
C. J. Burton <cjamesburton@outlook.com>
2022-01-20 01:04:34 +0000

    added basic calendar support with python's built in calendar module

--- calendar/main.py
+++ calendar/main.py
@@ -1,1 +1,17 @@
-from googleapiclient.discovery import build
+# Author C.J. Burton
+import calendar
+from datetime import date
+
+# Draw a quick calendar (no events)
+def drawCal():
+    current_date = date.today()
+    current_year = int(current_date.strftime("%y"))
+    current_month = int(current_date.strftime("%m"))
+    print(calendar.month(current_year, current_month))
+
+
+def main():
+    drawCal()
+
+if __name__ == "__main__":
+    main()
