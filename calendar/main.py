665cd17061f06c54264a022012102a99c37becc0
C. J. Burton <cjamesburton@outlook.com>
2022-01-26 11:58:56 +0000

    Added start to interactive mode menu

--- calendar/main.py
+++ calendar/main.py
@@ -1,20 +1,18 @@
 # Author C.J. Burton
 import csv
 import os.path
-from os import path, system, name
+from os import path, system, name, sys
 import calendar
 from datetime import date
 
 
-def interactiveMode(writer):
-    pass
 
-
-
+# Create an event and write to the events.csv file
 def createEvent(writer, eid, date, tstart, tend, title):
     writer.writerow([eid, date, tstart, tend, title])
 
 
+# TUI for creating an event
 def createEventInteractive(writer):
     # clear screen (UNIX only)
     system('clear')
@@ -28,34 +26,72 @@ def createEventInteractive(writer):
     print(date_print)
 
 
+# deletes an event and removes corresponding line in events.csv file
 def deleteEvent():
     # delete local event
     pass
 
 
+# modifies an event and writes the new line to the file
 def editEvent(event):
     # edit local event
     pass
 
 
+# list upcoming events for a specific time interval
 def listEvents():
+    # NYI
     # list upcoming events for a specific time interval
     # e.g.  listEvents(w) will return all events for the next 7 days
     #       listEvents(m) will return all events for the next 30 days
     #       listEvents() will return all events from the current date
     pass
 
+
+# look for a specific event in events.csv
 def searchEvents():
-    # look for a specific event
+    # NYI
     pass
 
 
-# Draw a quick calendar (no events)
-def drawCal():
-    current_date = date.today()
-    current_year = int(current_date.strftime("%y"))
-    current_month = int(current_date.strftime("%m"))
+# Draw a quick calendar (no events) 
+def drawCal(date):
+    current_year = int(date.strftime("%y"))
+    current_month = int(date.strftime("%m"))
     print(calendar.month(current_year, current_month))
+
+# interactive menu for when the user wants to do multiple things
+def menuInteractive(reader):
+    # Greeter and calendar info
+    system("clear")
+    print("""
+     ----- Welcome to Calcli! ------\n
+    A terminal Calendar application by C. J. Burton, any bugs/issues/feeback please
+    email me at cjamesburton@outlook.com
+    """)
+    drawCal(date.today())
+    print("\n\n")
+
+    """
+    # Controls
+    userSelection = {
+            "help"  : print(">HELP MESSAGE HERE<"),
+            "n"     : createEventInteractive(reader),
+            "d"     : deleteEventInteractive(reader),
+            "l"     : listEventsInteractive(reader),
+            "c"     : drawCal(date.today())
+    }
+    """
+
+    done = 0
+    while done != 0:
+        userInput = input("--->  ")
+        if userInput == "exit":
+            done = 1
+            exit()
+        # else ifs here
+        else:
+            print("Your input is:  " + userInput + "\n")
 
 
 def main():
@@ -74,12 +110,13 @@ def main():
     with open('events.csv') as csv_file:
         csvReader = csv.reader(csv_file, delimiter=',')
 
-    # --- CALENDAR MAINLOOP ---
+    # -- SYSTEM ARGS --
+    if len(sys.argv) == 1:
+        menuInteractive(csvReader)
+    else:
+        system("clear")
+        drawCal(date.today())
 
-    system("clear")
-    # draw a basic calendar for barebones functionality
-    drawCal()
-    createEventInteractive(csvReader)
 
 
 if __name__ == "__main__":
